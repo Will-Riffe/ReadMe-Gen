0a38600ce6776f3d7757f5fad557a66879881a16
Will Riffe <w_riffe@protonmail.com>
2023-04-16 20:56:28 -0400

    Finished changes to index.js; ran to make ReadMe

---
+++ README.md
@@ -0,0 +1,32 @@
+# ReadMe-Gen
+
+  ## Description
+  This is a "ReadMe.md" generator, meant to allow users to enter in their project descriptions, instillation, usage information, contribution instructions, and how to test the program.
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  Simply download a zip version of the file from my GitHub Profile, extract the contents, open it up in Git Bash, and type in "node index.js" to activate the appropriate file. From there, the program should prompt the user for the respective details of their project. 
+
+  ## Usage
+  For anyone who needs a ReadMe, this is the program for you. For everything else... Search for others.
+
+  ## License
+  ![MIT License](https://img.shields.io/badge/License-MIT-blue.svg)
+  This project is covered under the MIT License.
+
+  ## Contributing
+  None. Not Open to Contribution (sry)
+
+  ## Tests
+  Oops...I provided those elsewhere. Go back to instillation instructions... it's short enough to cover both really.
+
+  ## Questions
+  For questions or comments about this project, please contact me via [GitHub](https://github.com/Will-Riffe) or email at W_Riffe@protonmail.com.
+  
--- index.js
+++ index.js
@@ -2,31 +2,98 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer
-  .prompt([
-    {
+.prompt([
+  {
       type: 'input',
-      message: 'What is your name?',
-      name: 'name',
-    },
-    {
+      name: 'title',
+      message: 'What is the Title of your project?'
+  },
+  {
       type: 'input',
-      message: 'What languages do you know?',
-      name: 'languages',
-    },
-    {
+      name: 'description',
+      message: 'Please provide a Description of your project:'
+  },
+  {
       type: 'input',
-      message: 'What is your preferred method of communication?',
-      name: 'communication method',
-    },
-  ])
-  .then((response) => {
-    const jString = JSON.stringify(response);
-    fs.writeFile('output.json', jString, (err) => {
-        if (err) {
-        console.error(err);
-    } else {
-      console.log("Responses saved to output.json");
-    }  
-    });
-  });
+      name: 'installation',
+      message: 'Enter any installation instructions here:'
+  },
+  {
+      type: 'input',
+      name: 'usage',
+      message: 'Usage information for your project goes here:'
+  },
+  {
+      type: 'input',
+      name: 'contribution',
+      message: 'Enter contribution guidelines here:'
+  },
+  {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions for your project here:'
+  },
+  {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  },
+  {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?'
+  },
+  {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?'
+  }
+]).then((data) => {
 
+  // Code generating the README file (the magic happens here)
+
+  const readme = `# ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ![${data.license} License](https://img.shields.io/badge/License-${data.license}-blue.svg)
+  This project is covered under the ${data.license} License.
+
+  ## Contributing
+  ${data.contribution}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  For questions or comments about this project, please contact me via [GitHub](https://github.com/${data.github}) or email at ${data.email}.
+  `;
+
+  // Code for writing the README file to disk
+  fs.writeFile('README.md', readme, (err) => {
+      if (err) {
+          console.log(err);
+      } else {
+          console.log('README.md file created successfully!');
+      }
+  });
+}).catch((err) => {
+  console.log(err);
+});
